f1d99d769f41ab2f686c5748106273d2f5683bea
yuyu0127 <saiyuki01271210@gmail.com>
2021-02-16 18:22:46 +0900

    set icon

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        mac: {
+          icon: 'build/icon.png',
+        },
+        win: {
+          icon: 'build/icon.png',
+        }
+      }
+    }
+  }
+}
