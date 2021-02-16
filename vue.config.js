e09393afbe95f3dc3d9375eb9c952ce0bd19ec06
yuyu0127 <saiyuki01271210@gmail.com>
2021-02-16 18:38:45 +0900

    ビルド設定更新

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@ module.exports = {
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
+        productName: 'ToDo Always',
         mac: {
           icon: 'build/icon.png',
         },
         win: {
+          target: 'nsis',
           icon: 'build/icon.png',
-        }
+        },
       }
     }
   }
