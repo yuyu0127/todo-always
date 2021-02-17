357620f8a06728aad82f1e6c3b28df0d85718fd9
yuyu0127 <saiyuki01271210@gmail.com>
2021-02-18 03:08:01 +0900

    preload更新

--- public/preload.js
+++ public/preload.js
@@ -1,9 +1,11 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
 contextBridge.exposeInMainWorld(
-    "requires",
+    "electron",
     {
         fs: require("fs"),
-        ipcRenderer: ipcRenderer,
+        closeWindow: () => ipcRenderer.send('close-window'),
+        minimizeWindow: () => ipcRenderer.send('minimize-window'),
+        toggleAlwaysOnTop: () => ipcRenderer.invoke('toggle-always-on-top'),
     }
 );
