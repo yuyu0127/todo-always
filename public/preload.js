e9ec0279d4a971f004a47bdff8f1d299e7352d9e
yuyu0127 <saiyuki01271210@gmail.com>
2021-02-17 20:32:51 +0900

    gitキャッシュ削除

--- public/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron")
-
-contextBridge.exposeInMainWorld(
-    "requires",
-    {
-        fs: require("fs"),
-        ipcRenderer: ipcRenderer,
-    }
-);
